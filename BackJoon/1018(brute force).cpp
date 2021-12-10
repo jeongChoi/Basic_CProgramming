44a884ef495ca291cdd483ed86688e996736972d
jeongChoi <94841342+jeongChoi@users.noreply.github.com>
2021-12-10 17:23:38 +0900

    Add files via upload

---
+++ BackJoon/1018(brute force).cpp
@@ -0,0 +1,79 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+int replace(char** a) {
+	
+	int count = 0;
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 7; j++) {
+			if (i > 0 && j == 0) {
+				if (a[i - 1][j] == a[i][j]) {
+					if (a[i - 1][j] == 'B') {
+						a[i][j] = 'W';
+						count++;
+					}
+					else {
+						a[i][j] = 'B';
+						count++;
+					}
+				}
+			}
+			if (a[i][j] == a[i][j + 1]) {
+				if (a[i][j] == 'B') {
+					a[i][j + 1] = 'W';
+					count++;
+				}
+				else {
+					a[i][j + 1] = 'B';
+					count++;
+				}
+			}
+		}
+	}
+
+	return count;
+}
+
+int main(void) {
+	
+	int n, m;
+	int i, j, k, z;
+	char chess[51][51];
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%s", &chess[i]);
+	}
+
+	int cnt = 2501;
+	int count;
+
+	char **ch = (char **)malloc(sizeof(char*) * 8);
+	
+	for (int a = 0; a < 8; a++) {
+		ch[a] = (char*)malloc(sizeof(char) * 9);
+	}
+
+	for (i = 0; i <= n - 8; i++) {
+		for (j = 0; j <= m - 8; j++) {
+			int a = 0;
+			for (k = i; k < i + 8; k++, a++) {
+				int b = 0;
+				for (z = j; z < j + 8; z++, b++) {
+					ch[a][b] = chess[k][z];
+				}
+			}
+			count = replace(ch);
+			if (count < cnt) {
+				cnt = count;
+			}
+		}
+	}
+
+	printf("%d\n", cnt);
+	
+	return 0;
+}
